95064757965f57c43ff612df5199a4eceddc769a
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-04-12 17:19:19 -0500

    Fix typo in endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -24,7 +24,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
-	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activate", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
